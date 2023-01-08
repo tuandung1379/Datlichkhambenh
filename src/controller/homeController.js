aa119f4051794e49eb03f504736b43d44c6f421e
Dung Nguyen <tuandung.1379@gmail.com>
2023-01-08 15:43:01 +0700

    CRUD NodeJS- using ORM to edit data

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,6 @@
 import db from '../models/index';
 import CRUDService from '../services/CRUDService';
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -36,10 +37,36 @@ let displayGetCRUD = async (req, res) => {
     });
 }
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        //check user data not found
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else {
+        return res.send('Users not found!');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
