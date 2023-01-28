8828027bf6a4b91493d14cca808caccbbe47cc29
Nguyen Tuan Dung <tuandung.1379@gmail.com>
2023-01-28 12:33:13 +0700

    First API to check user's email

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express, { response } from "express";
 import homeController from "../controller/homeController";
+import userController from "../controller/userController"
 
 let router = express.Router();
 
@@ -12,6 +13,8 @@ let initWebRoutes = (app) => {
     router.get('/edit-crud', homeController.getEditCRUD);
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
+
+    router.post('/api/login', userController.handleLogin)
 
     return app.use("/", router);
 }
